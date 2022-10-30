7b8a4e09c4fa7f2b3a429551d24e4caed1f1ac6a
Ana <101293323+anaBequiman@users.noreply.github.com>
2022-10-30 12:59:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+//https://teachablemachine.withgoogle.com/models/vYptg1WYO/
+Webcam.set({
+    width:400,
+    height:300,
+    image_format:'png',
+    png_quality:90 
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+function captura(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("resulatado").innerHTML = '<img id="imagemCapturada"src="'+data_uri+'"/>';
+    });
+}
+console.log('ml5 version: ', ml5.version);
+classificador = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/vYptg1WYO/model.json', classificadordeImgens);
+function classificadordeImgens(){
+    console.log("Modelo Carregado :)");
+}
+function reconhecer(){
+    imagemIdentificada =  document.getElementById("imagemCapturada");
+    classificador.classify(imagemIdentificada,pegarResultado);
+}
+function pegarResultado(erro, resultado){
+    porcentagem = resultado[0].confidence.toFixed(3) * 100 + "%";
+    if (erro){
+        console.log(erro);
+    }
+    else{
+        console.log(resultado);
+        document.getElementById("pss").innerHTML = resultado[0].label ;
+        document.getElementById("pct").innerHTML= porcentagem;
+    }
+}
+function reconhecer(){
+    imagemIdentificada =  document.getElementById("imagemCapturada");
+    classificador.classify(imagemIdentificada,pegarResultado);
+}
+function pegarResultado(erro, resultado){
+    porcentagem = resultado[0].confidence.toFixed(3) * 100 + "%";
+    if (erro){
+        console.log(erro);
+    }
+    else{
+        console.log(resultado);
+        document.getElementById("pss").innerHTML = resultado[0].label ;
+        document.getElementById("pct").innerHTML= porcentagem;
+    }
+}
